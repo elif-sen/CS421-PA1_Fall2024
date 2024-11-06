2653cfc169c356229a2d3f280d62e3a19132b12f
Elif <91753310+elif-sen@users.noreply.github.com>
2024-11-06 19:58:04 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "Hello World"
+    return "Hello 22102144"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
